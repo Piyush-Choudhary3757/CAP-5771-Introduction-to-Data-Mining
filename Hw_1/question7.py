2665572dda25833ab5e86b748008b0125a489383
Piyush Choudhary <piyush99939@gmail.com>
2024-01-30 11:53:47 -0500

    test_1

---
+++ Hw_1/question7.py
@@ -0,0 +1,12 @@
+def question7_1():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_2():
+    answer = 'non-decreasing'
+    return answer
+
+def question7_3():
+    answer = [(0.0, 4.0), (4.0, 5.0), (5.0, 8.0), (8.0, 'infinity')]
+    return answer 
+
