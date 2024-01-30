9bb2259dbc82de16946d02e919045452c2d2a272
Piyush Choudhary <piyush99939@gmail.com>
2024-01-30 12:26:13 -0500

    test6

--- question6.py
+++ question6.py
@@ -1,20 +1,20 @@
 def question6_1():
     answer = {
-        'equal_width': ['change', 1],
-        'equal_frequency': ['change', 2]
+        'equal_width': ['no change', 3],
+        'equal_frequency': ['no change', 3]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['no change', 3],
-        'equal_frequency': ['change', 4]
+        'equal_width': ['no change', 7],
+        'equal_frequency': ['no change', 9]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['change', 5],
-        'equal_frequency': ['change', 6]
+        'equal_width': ['change', 2],
+        'equal_frequency': ['change', 9]
     }
     return answer
